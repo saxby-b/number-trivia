68d8d4caa906743bb8bbce7b6fbc4ec94300085c
C. Saxby Bain <csaxbybain@yahoo.com>
2026-01-31 12:51:41 -0500

    Formatted code

--- js/script.js
+++ js/script.js
@@ -1,33 +1,19 @@
 let numberTrivia = document.querySelector(".number-trivia");
-const triviaButton = document.querySelector(".trivia");
+const quoteButton = document.querySelector(".quote");
 const finishButton = document.querySelector(".finish");
 const change = document.querySelector(".change span");
 let triviaArray = [];
 let li = document.createElement("li");
 
 const fetchMotivationalQuote = async function () {
-  let res = await fetch("https://motivational-spark-api.vercel.app/api/quotes/random/10");
+  let res = await fetch(
+    "https://motivational-spark-api.vercel.app/api/quotes/random/10",
+  );
   let trivia = await res.json();
   console.log(trivia);
 };
 
-triviaButton.addEventListener("click", function () {
-  numberTrivia.classList.remove("hide");
-  fetchMotivationalQuote();
-  triviaButton.classList.add("hide");
-  finishButton.classList.remove("hide");
-});
-
-finishButton.addEventListener("click", function () {
-  numberTrivia.classList.add("hide");
-  triviaButton.classList.remove("hide");
-  finishButton.classList.add("hide");
-  for (trivia of triviaArray) {
-    if (triviaArray[trivia]) {
-      numberTrivia.remove(li);
-    }
-  }
-});
+quoteButton.addEventListener("click", fetchMotivationalQuote);
 
 let d = new Date();
 let year = d.getFullYear();
